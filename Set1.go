a1331b80e2cb4add722a86529a4e49dd758e409c
Racso01 <oscarmbarter@gmail.com>
2018-08-30 12:47:56 +0100

    Added solutions for Problem 2

--- Set1.go
+++ Set1.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"log"
+	"unicode/utf8"
 )
 
 func hexToBase64(hs string) (string, error) {
@@ -17,6 +18,23 @@ func hexToBase64(hs string) (string, error) {
 
 func xor(a, b []byte) []byte {
 	if len(a) != len(b) {
-		
+		panic("xor: mismatched lengths")
 	}
+	res := make([]byte, len(a))
+	for i := range a {
+		res[i] = a[i] ^ b[i]
+	}
+	return res
+}
+
+func buildCorpus(text string) map[rune]float64 {
+	c := make(map[rune]float64)
+	for _, char := range text {
+		c[char]++
+	}
+	total := utf8.RuneCountInString(text)
+	for char := range c {
+		c[char] = c[char] / float64(total)
+	}
+	return c
 }
--- Set1_test.go
+++ Set1_test.go
@@ -1,6 +1,10 @@
 package cryptopals
 
-import "testing"
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+)
 
 func TestProblem1(t *testing.T) {
 	res, err := hexToBase64 ("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
@@ -10,5 +14,28 @@ func TestProblem1(t *testing.T) {
 	if res != "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" {
 		t.Fatal("Wrong String", res)
 	}
+}
+
+func TestProblem2(t *testing.T) {
+	res := xor(hexDecode(t, "1c0111001f010100061a024b53535009181c"),
+		hexDecode(t, "686974207468652062756c6c277320657965"))
+	if !bytes.Equal(res, hexDecode(t, "746865206b696420646f6e277420706c6179")) {
+		t.Errorf("wrong result: %x", res)
+	}
+}
+func hexDecode(t *testing.T, s string) []byte {
+	v, err := hex.DecodeString(s)
+	if err != nil{
+		t.Fatal("Failed to decode hex:", s)
+	}
+	return v
+}
+
+func corpusFromFile(t *testing.T, ) map[rune]float64 {
+	text, err := ioutil.ReadFile(name)
+	if err != nil {
+		t.Fatal("failed to open corpus file:", err)
+	}
+	return buildCorpus(string(text))
 }
 
